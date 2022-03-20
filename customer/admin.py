ac6a2f84a98c6abf40308f7bd7efa1e0cb0239d7
mani <manikaustubh123123@gmail.com>
2022-03-20 19:24:45 +0530

    mani

---
+++ customer/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+# @admin.register(EcoProduct)
+# class EcoProductAdmin(admin.ModelAdmin):
+#     #list display
+#     list_display = ['name', 'price', 'category', 'date']
+#     #list Filter
+#     list_filter = ('category','date')
